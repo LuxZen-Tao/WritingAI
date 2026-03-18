fa24e9d810f6e1445eaa2ef83b36008067162d2e
LuxZen-Tao <22331820@bucks.ac.uk>
2026-03-18 16:31:25 +0000

    update

--- RememberedInteractable.cs
+++ RememberedInteractable.cs
@@ -3,16 +3,16 @@
 [System.Serializable]
 public class RememberedInteractable
 {
-	public Interactable interactable;
-	public NeedType needType;
-	public Vector3 lastKnownPosition;
-	public float lastSeenTime;
+    public Interactable interactable;
+    public NeedType needType;
+    public Vector3 lastKnownPosition;
+    public float lastSeenTime;
 
-	public RememberedInteractable(Interactable interactable, NeedType needType, Vector3 lastKnownPosition, float lastSeenTime)
-	{
-		this.interactable = interactable;
-		this.needType = needType;
-		this.lastKnownPosition = lastKnownPosition;
-		this.lastSeenTime = lastSeenTime;
-	}
+    public RememberedInteractable(Interactable interactable, NeedType needType, Vector3 lastKnownPosition, float lastSeenTime)
+    {
+        this.interactable = interactable;
+        this.needType = needType;
+        this.lastKnownPosition = lastKnownPosition;
+        this.lastSeenTime = lastSeenTime;
+    }
 }
---
+++ WorldArea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class WorldArea : MonoBehaviour
+{
+    [Header("World Settings")]
+    public string worldName = "World";
+
+    [Tooltip("Directional light / sun / global daylight source.")]
+    public Light worldLight;
+
+    public bool IsDaytime()
+    {
+        if (worldLight != null)
+        {
+            return worldLight.enabled;
+        }
+
+        return false;
+    }
+}
